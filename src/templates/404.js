8f32ac3cfbe42faf0d5b8ad058a1bfa4e93c4d89
Peter Koenders <pkoenders@gmail.com>
2020-11-08 22:00:46 +1300

    Update 404.js

--- src/templates/404.js
+++ src/templates/404.js
@@ -1,16 +1,16 @@
 import React from "react"
-
 import Layout from "../components/layout"
 //import SEO from "../components/seo"
 import { useTranslation } from "react-i18next"
 
-const NotFoundPage = () => {
-    const { t } = useTranslation("404")
+const NotFoundPage = ({ location }) => {
+    const { t, i18n } = useTranslation("404")
     return (
-        <Layout>
+        <Layout location={location}>
             {/* <SEO title={"404: " + t("notFound")} /> */}
-            <h1>{t("notFound")}</h1>
-            <p>{t("notFoundMessage")}</p>
+            <h1>Page not found placeholder</h1>
+            {/* <h1>{t("notFound")}</h1>
+            <p>{t("notFoundMessage")}</p> */}
         </Layout>
     )
 }
