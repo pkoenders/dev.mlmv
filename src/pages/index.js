424f0fd9a8c741a6d3ce91b1bb89b5b0b0f1e1f3
Peter Koenders <pkoenders@gmail.com>
2020-11-09 00:47:59 +1300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react"
 
 
 const MlmvIndex = ({ location }) => {
-  window.location = location.pathname + `/en`
+  // window.location = location.pathname + `/en`
   return (
     <>
 
