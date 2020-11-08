1010247f062ff3c9e42ce8500a86c707af17e052
Peter Koenders <pkoenders@gmail.com>
2020-11-08 23:05:24 +1300

    ss

--- gatsby-config.js
+++ gatsby-config.js
@@ -192,5 +192,9 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+
+
+    `gatsby-plugin-client-side-redirect` // keep it in last in list
+
   ],
 }
