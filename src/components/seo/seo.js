fb0251e424b4bc754265807373e76ea51ffbc544
Peter Koenders <pkoenders@gmail.com>
2020-11-25 16:06:01 +1300

    Update seo.js

--- src/components/seo/seo.js
+++ src/components/seo/seo.js
@@ -25,9 +25,10 @@ const SEO = ({ title, description, image, article }) => {
 
   return (
     <Helmet>
-      {/* <meta name="robots" content="index"></meta> */}
-      <meta name="robots" content="noindex"></meta>
       <html lang="en" />
+      <meta name="robots" content="index"></meta>
+      {/* <meta name="robots" content="noindex"></meta> */}
+
 
       {/* Need to define hreflang - See - https://web.dev/hreflang/?utm_source=lighthouse&utm_medium=devtools */}
       {/* <link rel="alternate" hreflang="en" href={`${siteUrl}${pathname}`} />
