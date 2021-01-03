71bffa00e352bf2aa1c27dc26a0332a27a43c12e
Peter Koenders <pkoenders@gmail.com>
2021-01-04 06:42:41 +1300

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -224,8 +224,23 @@ module.exports = {
     // `gatsby-plugin-offline`,
 
 
-    `gatsby-plugin-netlify`,
-    `gatsby-plugin-client-side-redirect` // keep it in last in list
+    //`gatsby-plugin-netlify`,
+
+    {
+      resolve: `gatsby-plugin-netlify`,
+      options: {
+        headers: {}, // option to add more headers. `Link` headers are transformed by the below criteria
+        allPageHeaders: [], // option to add headers for all pages. `Link` headers are transformed by the below criteria
+        mergeSecurityHeaders: true, // boolean to turn off the default security headers
+        mergeLinkHeaders: true, // boolean to turn off the default gatsby js headers
+        mergeCachingHeaders: true, // boolean to turn off the default caching headers
+        //transformHeaders: (headers, path) => headers, // optional transform for manipulating headers under each path (e.g.sorting), etc.
+        generateMatchPathRewrites: true, // boolean to turn off automatic creation of redirect rules for client only paths
+      },
+    },
+
+
+    //`gatsby-plugin-client-side-redirect` // keep it in last in list
 
   ],
 }
