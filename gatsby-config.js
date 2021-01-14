69f4dd9c1a8d99acaacb0112e4bf2145aef8387d
Peter Koenders <pkoenders@gmail.com>
2021-01-15 03:38:40 +1300

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -243,18 +243,18 @@ module.exports = {
 
     //`gatsby-plugin-netlify`,
 
-    {
-      resolve: `gatsby-plugin-netlify`,
-      options: {
-        headers: {}, // option to add more headers. `Link` headers are transformed by the below criteria
-        allPageHeaders: [], // option to add headers for all pages. `Link` headers are transformed by the below criteria
-        mergeSecurityHeaders: true, // boolean to turn off the default security headers
-        mergeLinkHeaders: true, // boolean to turn off the default gatsby js headers
-        mergeCachingHeaders: true, // boolean to turn off the default caching headers
-        //transformHeaders: (headers, path) => headers, // optional transform for manipulating headers under each path (e.g.sorting), etc.
-        generateMatchPathRewrites: true, // boolean to turn off automatic creation of redirect rules for client only paths
-      },
-    },
+    // {
+    //   resolve: `gatsby-plugin-netlify`,
+    //   options: {
+    //     headers: {}, // option to add more headers. `Link` headers are transformed by the below criteria
+    //     allPageHeaders: [], // option to add headers for all pages. `Link` headers are transformed by the below criteria
+    //     mergeSecurityHeaders: true, // boolean to turn off the default security headers
+    //     mergeLinkHeaders: true, // boolean to turn off the default gatsby js headers
+    //     mergeCachingHeaders: true, // boolean to turn off the default caching headers
+    //     //transformHeaders: (headers, path) => headers, // optional transform for manipulating headers under each path (e.g.sorting), etc.
+    //     generateMatchPathRewrites: true, // boolean to turn off automatic creation of redirect rules for client only paths
+    //   },
+    // },
     `gatsby-plugin-preact`
 
     // `gatsby-plugin-client-side-redirect` // keep it in last in list
