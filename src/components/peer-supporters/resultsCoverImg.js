37c49b46738c45e773f0838cac4f70ec28e8f9b7
Peter Koenders <pkoenders@gmail.com>
2021-01-22 11:18:25 +1300

    Update resultsCoverImg.js

--- src/components/peer-supporters/resultsCoverImg.js
+++ src/components/peer-supporters/resultsCoverImg.js
@@ -6,16 +6,17 @@ const ResultsCoverImge = ({ edge }) => {
 
     //console.log("edge.node.coverImage.asset = " + edge.node.coverImage.asset)
     return (
+
         <>
-            {edge.node.coverImage.asset !== null
-                ? <i>
+            {edge.node.coverImage.asset === null
+                ? ''
+                : <>
                     <Img
                         fluid={edge.node.coverImage.asset.fluid}
                         loading="lazy"
                     />
                     <span className={listResults.resultsContentImgOverlay}></span>
-                </i>
-                : ''
+                </>
             }
         </>
     )
